311074b0244ea2696451d39f28796f95ad64058a
Tanay Agrawal <tagrawa3@ncsu.edu>
2020-03-05 23:35:55 -0500

    final commit

--- adwords.py
+++ adwords.py
@@ -1,16 +1,15 @@
-import csv
+import sys
 import copy
+import math 
 import random
 import pandas as pd
-
+import numpy as np
 def readFile(filename):
 	with open(filename) as fp:
 		return fp.read().splitlines()
 
 def readCSV(filename):
-	#reader = csv.reader(open(filename, 'r'))
 	reader = pd.read_csv(filename)
-	count = 0
 	data = []
 	n = len(reader)
 	for i in range(0, n):
@@ -29,55 +28,137 @@ def getBidderBudget(bidderData):
 def getBidderQueries(bidderData):
 	bidderQueries = dict()
 	for row in bidderData:
-		bidderQueries[(row[0], row[1])] = row[2]
+		bidderQueries[row[1]] = list()
+	for row in bidderData:
+		bidderQueries[row[1]].append((row[0], row[2]))
 	return bidderQueries
 
 def getOptimalMatching(bidderBudget):
 	total = 0
 	for key in bidderBudget:
-		total += int(bidderBudget[key])
+		total += bidderBudget[key]
 	return total
 
 def greedyAlgo(queriesData, bidderBudget, bidderQueries):
 	totalRevenue = 0
 	for query in queriesData:
-		for key in bidderQueries:
-			tempMax = 0
-			tempId = -1
-			if key[1] == query:
-				value = bidderQueries[key]
-				if value > bidderBudget[key[0]]:
-					continue
-				elif value > tempMax:
-					break
-					tempMax = value
-					tempId = key[0]
-					
-		print(tempMax)
-		print(tempId)
-		totalRevenue += tempMax
-		bidderBudget[key[0]] -= tempMax
+		tempMax = 0
+		tempId = -1
+		newBids = bidderQueries[query]
+		for aId, bidValue in newBids:
+			if bidValue > tempMax:
+				if bidderBudget[aId] >= bidValue:
+					tempMax = bidValue
+					tempId = aId
+		#for key in bidderQueries:  # (id, query) : valueBid
+		#	if key[1] == query:
+		#		value = bidderQueries[key]
+		#		if value > bidderBudget[key[0]]:
+		#			continue
+		#		elif value > tempMax:
+		#			tempMax = value
+		#			tempId = key[0]
+		#totalRevenue += tempMax
+		if tempId != -1:
+			totalRevenue += tempMax
+			bidderBudget[tempId] -= tempMax
 	return totalRevenue
+
+def balanceAlgo(queriesData, bidderBudget, bidderQueries):
+	totalRevenue = 0
+	for query in queriesData:
+		tempMax = 0
+		tempId = -1
+		tempBid = 0
+		newBids = bidderQueries[query]
+		for aId, bidValue in newBids:
+			if bidderBudget[aId] > tempMax:
+				if bidderBudget[aId] >= bidValue:
+					tempMax = bidderBudget[aId]
+					tempId = aId
+					tempBid = bidValue
+		if tempId != -1:
+			totalRevenue += tempBid
+			bidderBudget[tempId] -= tempBid
+	return totalRevenue
+
+def mathCalc(value):
+	temp = 1 - math.exp(value - 1)
+	return temp
+
+def msvvAlgo(queriesData, bidderBudget1, bidderBudget2, bidderQueries):
+	totalRevenue = 0
+	for query in queriesData:
+		tempMax = 0
+		tempId = -1
+		tempBid = 0
+		newBids = bidderQueries[query]
+		for aId, bidValue in newBids:
+			new = bidValue * mathCalc((bidderBudget2[aId] - bidderBudget1[aId])/bidderBudget2[aId])
+			if new > tempMax:
+				if bidderBudget1[aId] >= bidValue:
+					tempMax = new
+					tempId = aId
+					tempBid = bidValue
+		if tempId != -1:
+			totalRevenue += tempBid
+			bidderBudget1[tempId] -= tempBid
+	return totalRevenue
+
+def getALTMSSVAlgo(queriesData, bidderBudget, bidderQueries):
+	totalBidValue = 0
+	random.seed(0)
+	tempBidderBudget = copy.deepcopy(bidderBudget)
+	tempBidValue = msvvAlgo(queriesData, tempBidderBudget, bidderBudget, bidderQueries)
+	print(round(tempBidValue,2))
+	for i in range(0, 100):
+		random.shuffle(queriesData)
+		tempBidderBudget = copy.deepcopy(bidderBudget)
+		tempBidValue = msvvAlgo(queriesData, tempBidderBudget, bidderBudget, bidderQueries)
+		totalBidValue += tempBidValue
+	return totalBidValue/100
+
+def getALTBalanceAlgo(queriesData, bidderBudget, bidderQueries):
+	totalBidValue = 0
+	random.seed(0)
+	tempBidderBudget = copy.deepcopy(bidderBudget)
+	tempBidValue = balanceAlgo(queriesData, tempBidderBudget, bidderQueries)
+	print(round(tempBidValue,2))
+	for i in range(0, 100):
+		random.shuffle(queriesData)
+		tempBidderBudget = copy.deepcopy(bidderBudget)
+		tempBidValue = balanceAlgo(queriesData, tempBidderBudget, bidderQueries)
+		totalBidValue += tempBidValue
+	return totalBidValue/100
 
 def getALTGreedyAlgo(queriesData, bidderBudget, bidderQueries):
 	totalBidValue = 0
 	random.seed(0)
-	for i in range(0, 1):
+	tempBidderBudget = copy.deepcopy(bidderBudget)
+	tempBidValue = greedyAlgo(queriesData, tempBidderBudget, bidderQueries)
+	print(round(tempBidValue,2))
+	for i in range(0, 100):
 		random.shuffle(queriesData)
 		tempBidderBudget = copy.deepcopy(bidderBudget)
 		tempBidValue = greedyAlgo(queriesData, tempBidderBudget, bidderQueries)
-		print(tempBidValue)
 		totalBidValue += tempBidValue
 	return totalBidValue/100
 
 def main():
+	algorithm = str(sys.argv[1])
 	queriesData = readFile('queries.txt')
 	bidderData = readCSV('bidder_dataset.csv')
 	bidderBudget = getBidderBudget(bidderData)
 	bidderQueries = getBidderQueries(bidderData)
 	oPT = getOptimalMatching(bidderBudget)
-	print(oPT)
-	aLT = getALTGreedyAlgo(queriesData, bidderBudget, bidderQueries)
-	print(aLT)
+	if algorithm == "greedy":
+		aLT = getALTGreedyAlgo(queriesData, bidderBudget, bidderQueries)
+		print(round(aLT/oPT,2))
+	elif algorithm == "balance":
+		aLT = getALTBalanceAlgo(queriesData, bidderBudget, bidderQueries)
+		print(round(aLT/oPT,2))
+	elif algorithm == "msvv" or algorithm == "mssv":
+		aLT = getALTMSSVAlgo(queriesData, bidderBudget, bidderQueries)
+		print(round(aLT/oPT,2))
 
 main()
